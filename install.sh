8119e0ee5d8b3110f03da201d4c7b0b24e5f1c07
Космос Преда́ние <kosmospredanie@yandex.ru>
2019-09-01 18:29:22 +0300

    feat: Add expvpn-iptables-block

--- install.sh
+++ install.sh
@@ -5,8 +5,11 @@ set -o xtrace
 mkdir -p /usr/local/bin
 mkdir -p /usr/local/share/applications
 mkdir -p /etc/xdg/autostart
-cp expvpn-iptables-flush expvpn-iptables-setup \
-	try-expvpn-connect expvpn-connect \
+cp expvpn-iptables-flush \
+	expvpn-iptables-block \
+	expvpn-iptables-setup \
+	try-expvpn-connect \
+	expvpn-connect \
 	/usr/local/bin
 cp try-expvpn-connect.desktop /usr/local/share/applications
 ln -s /usr/local/share/applications/try-expvpn-connect.desktop /etc/xdg/autostart
