f272c21ba689e4b82f749d37d84b59f0e47b28bc
Космическое П <kosmospredanie@yandex.ru>
2020-02-05 03:07:05 +0300

    refactor: Update install script

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ set -o xtrace
 mkdir -p /usr/local/bin
 mkdir -p /usr/local/share/applications
 mkdir -p /etc/xdg/autostart
-cp expvpn-iptables-flush \
+cp -ip expvpn-iptables-flush \
 	expvpn-iptables-block \
 	expvpn-iptables-setup \
 	try-expvpn-connect \
 	expvpn-connect \
 	/usr/local/bin
-cp try-expvpn-connect.desktop /usr/local/share/applications
-ln -sf /usr/local/share/applications/try-expvpn-connect.desktop /etc/xdg/autostart
+cp -ip try-expvpn-connect.desktop /usr/local/share/applications
+ln -si /usr/local/share/applications/try-expvpn-connect.desktop /etc/xdg/autostart
